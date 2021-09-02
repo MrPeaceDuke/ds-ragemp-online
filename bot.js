8ddd1e995f090165f5b0cb3f222883b389b8dba6
MrPeaceDuke <daemonica120@yandex.ru>
2021-09-02 15:30:40 +0500

    v 0.1

---
+++ bot.js
@@ -0,0 +1,73 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+// const { joinVoiceChannel } = require('@discordjs/voice');
+const config = require("./config.json")
+const token = config.token;
+// const cheerio = require('cheerio');
+
+var webscrape = require('webscrape');
+var scraper = webscrape.default();
+
+let online = "0/1000"; //не трогать, максимальный онлайн изменится автоматически
+
+let myServerName = "CCDPlanet | discord.gg/ccdplanet";//изменить на своё, можно указать часть названия своего сервера
+
+var timer = null;
+
+client.login(token);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(client.user.username);
+  timer = setInterval(() => {
+    UpdateBotName();
+  }, 20000);
+});
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+client.on("messageCreate", message => {
+  if (message.content === "test") {
+    let cache = message.guild.channels.cache;
+    console.log(cache.map((channel) => channel.name).join(","));
+  }
+
+  if (message.channel.name === "bot") {
+    let countMessages = 0;
+    message.channel.messages.fetch().then((messages) => {
+      countMessages = messages.size;
+      message.channel.bulkDelete(countMessages).then(() => {
+        // message.channel.send("Del")
+      })
+        .catch(console.error)
+    });
+  }
+})
+
+function UpdateBotName() {
+  scrapOnline();
+}
+
+async function scrapOnline() {
+  const result = await scraper.get('https://cdn.rage.mp/master/');
+  const servers = JSON.parse(result.body);
+  let server = Object.values(servers).find((el) => el.name.includes(myServerName) === true);
+  if (server !== undefined) {
+    online = server.players + "/" + server.maxplayers;
+    UpdateChannelName(online);
+  }
+}
+
+function UpdateChannelName(tempOnline = "0/1000") {
+  let cache = client.channels.cache;
+  let channelElement = cache.find((el) => el.name.includes("Онлайн:") === true);
+  const channel = client.channels.cache.get(channelElement.id);
+  if (!channel) return console.error("Канал или Категория не найдены");
+  channel.setName("Онлайн: " + tempOnline)
+}
